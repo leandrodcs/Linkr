590eeec5c4a90dd214a4556c971a821004f17a3f
CarlosEFPaiva <carlosedufpaiva@poli.ufrj.br>
2021-09-14 12:04:12 -0300

    instaled libs: Axios, styled-components and react-router-dom / Added empty files on each folder, so it can be uploaded to GitHub / Added a 'temp_mocks' folder, so developers can simulate features that are yet to be implemented, but are important for tasks that are being done.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 export default function App() {
   return (
-    <>
-    Base project created and deployed
-    </>
+    <Router>
+      <Switch>
+        <Route path = "/" exact>
+          Base project created and deployed
+        </Route>
+      </Switch>
+    </Router>
   );
 }
