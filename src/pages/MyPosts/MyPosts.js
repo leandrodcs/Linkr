d0c19adc91548f6d5ccb877a3f72402f7a6e6a54
leandrodcs <eng.leandrodcs@gmail.com>
2021-09-15 12:09:26 -0300

    Message added if user hasn't created any posts yet

--- src/pages/MyPosts/MyPosts.js
+++ src/pages/MyPosts/MyPosts.js
@@ -13,9 +13,11 @@ import UserContext from "../../contexts/UserContext";
 
 export default function MyPosts() {
     const {user} = useContext(UserContext);
-    const [userPosts, setUserPosts] = useState(MockPosts.posts);
+    const [userPosts, setUserPosts] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
+        setLoading(true);
         const config = {
           headers: {
             Authorization: `Bearer ${user.token}`
@@ -24,12 +26,13 @@ export default function MyPosts() {
         axios.get(`https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/users/${user.user.id}/posts`, config)
         .then(res => {
           console.log(res);
+          setLoading(false);
           //setUserPosts(res.data);
         })
         .catch(err => alert(err));
       }, []);
 
-    if(!userPosts) {
+    if(loading) {
         return (
             <Container>
                 <Loading />
@@ -43,13 +46,12 @@ export default function MyPosts() {
             <Header />
             <Wrapper>
                 <PageTitle text = "my posts" />
-                {userPosts.map( ({id, text, user, likes}) => 
-                    <Post 
-                        key = {id}
-                        text = {text}
-                        user = {user}
-                        likes = {likes}
-                    />)
+                {
+                userPosts.length 
+                ?
+                userPosts.map( ({id, text, user, likes}) => <Post key={id} text={text} user={user} likes={likes}/>) 
+                :
+                <p>Você ainda não criou nenhum post!</p>
                 }
             </Wrapper>
             <Trending />
@@ -59,6 +61,12 @@ export default function MyPosts() {
 
 const Wrapper = styled.section`
     width: 611px;
+
+    p {
+        font-family: 'Lato', sans-serif;
+        font-size: 20px;
+        color: #151515;
+    }
     
     @media(max-width: 937px) {
         width: 100%;
