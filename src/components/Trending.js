ad5e5a63f52ef7fb6ae0eb73beb4d53d11237c55
CarlosEFPaiva <carlosedufpaiva@poli.ufrj.br>
2021-09-16 10:47:21 -0300

    implemented: finished layout for Trending box, already making hashtags highlighted and clickable. For now, still using mock Hashtags

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -1,9 +1,15 @@
+import { TextWithHighlightedHashtags } from "../utils/TextAdjustmentsUtils";
+
+import { useState } from "react";
 import styled from "styled-components";
 
 export default function Trending() {
+    const example = ["javascript", 'react', 'react-native', 'material', "web-dev", 'mobile', 'css', 'html', 'node', 'sql']
+    const [trendingTopics, setTrendingTopics ] = useState(example);
     return (
         <Wrapper>
-            Trending
+            <Title> trending </Title>
+            {PostHashtags(trendingTopics)}
         </Wrapper>
     );
 
@@ -25,5 +31,35 @@ const Wrapper = styled.section`
     @media(max-width: 937px) {
         display: none;
     }
-
 `
+
+const Title = styled.div`
+    width: 100%;
+    height: 61px;
+    display: flex;
+    align-items: center;
+    padding-left: 16px;
+    margin-bottom: 24px;
+    border-bottom: 1px solid #484848;
+    font-size: 27px;
+    font-weight: 700;
+    color: #FFF;
+`
+
+const Hashtags = styled.p`
+    font-size: 19px;
+    font-weight: 700;
+    color: #FFF;
+    padding-left: 16px;
+    margin-bottom: 7px;
+`
+
+function PostHashtags(hashtags){
+    return hashtags.map( (hashtag,index) => 
+        <TextWithHighlightedHashtags
+            key = { index }
+            text = {`#${hashtag}`}
+            HashtagStyledComponent = {Hashtags}
+        />
+    );
+}
