5efeda3464d40cb51582ed2e9304bd9d82a54a49
Gabriel Torres <al2012.163.torres@gmail.com>
2021-09-16 19:17:14 -0300

    Changed API address

--- src/service/service.js
+++ src/service/service.js
@@ -3,7 +3,7 @@ import { saveToLocalStorage } from "../utils/localStorageUtils";
 
 import axios from "axios";
 
-const URL = "https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr";
+const URL = "https://mock-api.bootcamp.respondeai.com.br/api/v3/linkr";
 
 function createConfig(userToken) {
     const config = {
