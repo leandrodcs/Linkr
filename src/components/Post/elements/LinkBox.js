a277b565b59f151f95229ecb3876117b3ff0f7e2
Gabriel Torres <al2012.163.torres@gmail.com>
2021-09-22 00:36:03 -0300

    Implemented youtube player

--- src/components/Post/elements/LinkBox.js
+++ src/components/Post/elements/LinkBox.js
@@ -4,9 +4,24 @@ import { CheckTextLengthAndReduceItIfNeeded } from "../../../utils/TextAdjustmen
 
 import styled from "styled-components";
 import { useContext } from "react";
+import getYouTubeID from "get-youtube-id";
 
 export default function LinkBox() {
     const { link, linkTitle, linkDescription, linkImage } = useContext(PostContext);
+
+    const youtubeId = getYouTubeID(link)
+
+    if(youtubeId) {
+        return (
+            <Wrapper video={true}>
+                <iframe
+                    src={"https://www.youtube.com/embed/" + youtubeId}
+                />
+                <p onClick = {() => OpenLinkInNewPage(link)}>{link}</p>
+            </Wrapper>
+        );
+    }
+
     return (
         <Wrapper onClick = {() => OpenLinkInNewPage(link)}>
             <LinkBoxContent>
@@ -20,10 +35,10 @@ export default function LinkBox() {
 }
 
 const Wrapper = styled.div`
-    width: 100%;
-    border: 1px solid #C4C4C4;
-    border-radius: 11px;
-    display: flex;
+    width: 500px;
+    border: ${props => props.video ? "none" : "1px solid #C4C4C4"};
+    border-radius: ${props => props.video ? "0px" : "11px"};
+    display: ${props => props.video ? "initial" : "flex"};
     justify-content: space-between;
     cursor: pointer;
     & img {
@@ -32,10 +47,24 @@ const Wrapper = styled.div`
         object-fit: cover;
         border-radius: 0px 11px 11px 0px;
     }
+    & iframe {
+        width: 500px;
+        height: 281px;
+    }
+    p {
+        font-family: Lato;
+        font-size: 17px;
+        font-weight: 400;
+        color: #B7B7B7;
+        margin: 10px 0px 4px;
+    }
     @media(max-width: 637px) {
         & img {
         width: 100px;
         height: 100px;
+        }
+        & iframe {
+            height: calc((100vw / 16) * 9);
         }
     }
 `
